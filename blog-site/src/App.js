084183dd3669012068d193b785e17181f34637ec
girivivek45 <vivek.21ug2020@iiitranchi.ac.in>
2023-08-16 19:38:41 +0530

    header file added

--- blog-site/src/App.js
+++ blog-site/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar/Navbar';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home/>
+
     </div>
   );
 }
