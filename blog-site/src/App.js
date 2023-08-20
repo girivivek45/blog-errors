e590d118cf9ea89e352852ad38352312107061da
girivivek45 <vivek.21ug2020@iiitranchi.ac.in>
2023-08-20 15:28:37 +0530

    write blog feature added

--- blog-site/src/App.js
+++ blog-site/src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import Navbar from './Navbar/Navbar';
 import Home from './Pages/Home/Home';
 import Single from './Pages/Single/Single';
+import Write from './Pages/Write/Write';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Single/>
+      <Write/>
 
     </div>
   );
