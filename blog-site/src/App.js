9bc96862412513d7b2787f1d96283c0267e6a0ea
girivivek45 <vivek.21ug2020@iiitranchi.ac.in>
2023-08-21 17:14:56 +0530

    login page added

--- blog-site/src/App.js
+++ blog-site/src/App.js
@@ -1,16 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
+
 import Navbar from './Navbar/Navbar';
 import Home from './Pages/Home/Home';
 import Single from './Pages/Single/Single';
 import Write from './Pages/Write/Write';
 import Settings from './Pages/Settings/Settings';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Settings/>
+      <Login/>
 
     </div>
   );
